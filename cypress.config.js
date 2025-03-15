e7d8b96e406ad4b6390cb8b377cbf6d0d9735d74
Cesar Isturiz <isturizcesar5@gmail.com>
2025-03-15 16:57:51 -0300

    i add the chromeWebSecurity property to be able to change the domain in a test

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: 'jsmski',
   e2e: {
+    chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
